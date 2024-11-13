7a3997dbd3b2066d3553f335c21afaa71e04f70d
Dino2343 <dinodizdarevic2001@gmail.com>
2024-11-13 18:45:29 +0100

    Update nogomet.sql

--- nogomet.sql
+++ nogomet.sql
@@ -11,7 +11,7 @@ go
 
 create table igrac(
 sifra int not null primary key identity (1,1),
-ime ime varchar (50) not null,
+ime varchar (50) not null,
 prezime varchar (50) not null,
 datumrodenja datetime not null,
 pozicija varchar (30),
@@ -20,12 +20,30 @@ klub varchar (50)
 );
 
 create table trener(
-sifra int 
-ime ime varchar (50) not null,
+sifra int not null primary key identity (1,1),
+ime varchar (50) not null,
 prezime varchar (50) not null,
 klub varchar (50),
 nacionalnost varchar (40),
 iskustvo int
 );
 
-create table utakmica 
+create table utakmica (
+sifra int not null primary key identity (1,1),
+datum datetime not null, 
+vrijeme time not null,
+lokacija varchar (50) not null,
+stadion varchar (50),
+domaciklub varchar (50),
+gostojuciklub varchar (50)
+);
+
+create table klub (
+sifra int not null primary key identity (1,1),
+naziv varchar (50)not null  ,
+osnovan int ,
+stadion varchar (50),
+predsjednik varchar (50) not null,
+drzava varchar (50),
+liga varchar (50)
+);
