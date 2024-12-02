6f60f2fc1ab64075c246c772679aa1fe7c467702
Dino2343 <dinodizdarevic2001@gmail.com>
2024-12-02 18:51:36 +0100

    UlaziIzlaz

---
+++ CSHARP/CSHARP/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+            //izlaz
+            Console.WriteLine("Hello");
+
+            //ulaz
+            string ime = Console.ReadLine();
+            Console.WriteLine(ime);
+        }
+
+
+    }
+}
--- CSHARP/CSHARP/Ucenje/Program.cs
+++ CSHARP/CSHARP/Ucenje/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine¸("Pozdrav svijetu");
-Console.WriteLine("Hello Osijek");
+﻿using Ucenje;
+E01UlazIzlaz.Izvedi();
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine¸("Pozdrav svijetu");
+//Console.WriteLine("Hello Osijek");
