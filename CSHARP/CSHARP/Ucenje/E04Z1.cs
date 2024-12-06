4652c0ea04092d45a5a123c0d0b773857e64d602
Dino2343 <dinodizdarevic2001@gmail.com>
2024-12-06 18:00:47 +0100

    E04Z1

---
+++ CSHARP/CSHARP/Ucenje/E04Z1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E04Z1
+    {
+        public static void Izvedi()
+        {
+            Console.Write("Unesite broj godina: ");
+            int godine = int.Parse(Console.ReadLine());
+            if (godine < 0 | godine > 112)
+            {
+                Console.WriteLine("GREŠKA");
+                return;
+            }
+            if (godine >= 18 )
+            {
+                Console.WriteLine("Punoljetan");
+
+            }
+            else
+            {
+                Console.WriteLine("Maloljetan");
+            }
+          
+
+
+        }
+    }
+}
