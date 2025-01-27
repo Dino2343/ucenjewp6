d087e1ee1e725f4e5923a908bdc7cfc230bfcd38
Dino2343 <dinodizdarevic2001@gmail.com>
2025-01-27 18:01:16 +0100

    Priprema za smjer read

---
+++ CSHARP/CSHARP/EdunovaAPP/Data/EdunovaContext.cs
@@ -0,0 +1,15 @@
+﻿using EdunovaAPP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Contracts;
+
+namespace EdunovaAPP.Data
+{
+    public class EdunovaContext:DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext>options):base(options)
+        {
+
+        }
+        public DbSet<Smjer>Smjerovi { get; set; }
+    }
+}
--- CSHARP/CSHARP/EdunovaAPP/Program.cs
+++ CSHARP/CSHARP/EdunovaAPP/Program.cs
@@ -5,18 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// dodati ovu liniju za swagger
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// dodati ove dvije linije za swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
